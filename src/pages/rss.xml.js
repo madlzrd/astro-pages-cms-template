f4909bf584136ef21b7b9d78959ea3f461b68058
Tom Peyser <tom.peyser@op3ft.org>
2025-12-01 21:08:48 +0100

    personnalisations pour le jardin

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,14 @@ import { getCollection } from 'astro:content';
 import { title as SITE_TITLE, description as SITE_DESCRIPTION } from '../config/site.json';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
+	const posts = await getCollection('articles');
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
-			link: `/blog/${post.id}/`,
+			link: `/articles/${post.id}/`,
 		})),
 	});
 }
